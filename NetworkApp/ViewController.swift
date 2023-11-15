77e0e871122576c1f386fa18a18596df44236fb3
Grigory Sapogov <grisha.sapgv@mail.ru>
2023-11-15 14:45:13 +0500

    Initial Commit

---
+++ NetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp
+//
+//  Created by Grigory Sapogov on 15.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
